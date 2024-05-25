9d1037e57313489b54630cf1addfb6c71b2d25f0
cord-burmeister <cord.burmeister@live.de>
2024-05-25 17:36:03 +0200

    Add parsing to the script and optional SDF output

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pymeshlab
+import argparse
 
 
-def calculate_inertial_tag(file_name=None, mass=-1, pr=8, scale_factor=100):
+def calculate_inertial_tag(file_name=None, mass=-1, pr=8, scale_factor=100, urdf_output=True):
     ms = pymeshlab.MeshSet()
 
     if file_name is None:
@@ -28,9 +29,40 @@ def calculate_inertial_tag(file_name=None, mass=-1, pr=8, scale_factor=100):
     volume = geom['mesh_volume']
     tensor = geom['inertia_tensor'] / pow(scale_factor, 2) * mass / volume
 
-    intertial_xml = f'<inertial>\n  <origin xyz="{com[0]:.{pr}f} {com[1]:.{pr}f} {com[2]:.{pr}f}"/>\n  <mass value="{mass:.{pr}f}"/>\n  <inertia ixx="{tensor[0, 0]:.{pr}f}" ixy="{tensor[1, 0]:.{pr}f}" ixz="{tensor[2, 0]:.{pr}f}" iyy="{tensor[1, 1]:.{pr}f}" iyz="{tensor[1, 2]:.{pr}f}" izz="{tensor[2, 2]:.{pr}f}"/>\n</inertial>'
-    print(intertial_xml)
+    if urdf_output:
+        intertial_xml = f'<inertial>\n  <origin xyz="{com[0]:.{pr}f} {com[1]:.{pr}f} {com[2]:.{pr}f}"/>\n  <mass value="{mass:.{pr}f}"/>\n  <inertia ixx="{tensor[0, 0]:.{pr}f}" ixy="{tensor[1, 0]:.{pr}f}" ixz="{tensor[2, 0]:.{pr}f}" iyy="{tensor[1, 1]:.{pr}f}" iyz="{tensor[1, 2]:.{pr}f}" izz="{tensor[2, 2]:.{pr}f}"/>\n</inertial>'
+        print(intertial_xml)
+
+    if not urdf_output:
+        intertial_xml = f'<inertial>\n  <pose> {com[0]:.{pr}f} {com[1]:.{pr}f} {com[2]:.{pr}f} 0 0 0 </pose>\n  <mass>{mass:.{pr}f}</mass>\n  <inertia>\n  <inertia>\n   <ixx>{tensor[0, 0]:.{pr}f}</ixx> \n   <ixy>{tensor[1, 0]:.{pr}f}</ixy>\n   <ixz>{tensor[2, 0]:.{pr}f}</ixz>\n   <iyy>{tensor[1, 1]:.{pr}f}</iyy>\n   <iyz>{tensor[1, 2]:.{pr}f}</iyz>\n   <izz>{tensor[2, 2]:.{pr}f} </izz>\n  </inertia>\n</inertial>'
+        print(intertial_xml)
 
 
 if __name__ == '__main__':
-    calculate_inertial_tag()  # TODO command line arguments
+
+    file_name=None 
+    mass=-1 
+    pr=8 
+    scale_factor=100
+    urdf_output=True
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_name', dest='file_name', type=str, help='Add filename of the DAE or STL file to calculate the inertial from')
+    parser.add_argument('--mass', dest='mass', type=float, nargs='?', const=1.0, help='Add the mass of the object')
+    parser.add_argument('--scale_factor', dest='scale_factor', nargs='?', const=100.0, type=float, help='Define the internal scale factor to improve precision')
+    parser.add_argument('--float_precision', dest='float_precision', nargs='?', const=8, type=float, help='Define the precision of the float in the output')
+    parser.add_argument('--urdf_output', dest='urdf_output', type=str, choices=['True', 'False'], nargs='?', const="True", help='flag indicating if output is urdf or sdf format')
+    args = parser.parse_args()
+
+    print (args)
+
+    if args.mass is not None:
+        mass = args.mass
+    if args.scale_factor is not None:
+        scale_factor = args.scale_factor
+    if args.float_precision is not None:
+        pr = args.float_precision
+    if args.urdf_output is not None:
+        urdf_output = args.urdf_output == "True"
+
+    calculate_inertial_tag(file_name = args.file_name, mass=mass, scale_factor=scale_factor, pr =pr, urdf_output = urdf_output )  
